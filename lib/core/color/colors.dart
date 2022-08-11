f4558e7f9dada8ed7b373c51ef53425408d4898e
MushthakVP <mail.musthak@gmai.com>
2022-08-11 17:01:29 +0530

    Carousel create

--- lib/core/color/colors.dart
+++ lib/core/color/colors.dart
@@ -5,3 +5,4 @@ const secondaryColor = Color(0xFFffd018);
 const blackColor = Color(0xFF000000);
 const whiteColor = Color(0xFFFFFFFF);
 const greyColor = Color(0xFF9E9E9E);
+const scafoldColor =  Color(0xffcbcbcb);
--- lib/home_screen/view/home_screen.dart
+++ lib/home_screen/view/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/core/color/colors.dart';
+import 'package:food_delivery/core/styles/fonts.dart';
+import 'package:food_delivery/home_screen/view/widget/carousel_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,6 +9,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: scafoldColor,
       drawer: const Drawer(),
       appBar: AppBar(
         backgroundColor: primaryColor,
@@ -20,6 +23,7 @@ class HomeScreen extends StatelessWidget {
               color: blackColor,
             ),
           ),
+          SizedBox(width: 5),
           CircleAvatar(
             backgroundColor: Color(0xFFDAD67C),
             child: Icon(
@@ -32,19 +36,45 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            margin: const EdgeInsets.all(10),
-            height: 180,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              image: const DecorationImage(
-                image: NetworkImage('https://t4.ftcdn.net/jpg/01/43/88/31/360_F_143883132_bn9n14k3aX10bq5HN18IYHPbx9YyiSEA.jpg'),
-                fit: BoxFit.cover,
-              ),
-              color: primaryColor,
-              borderRadius: BorderRadius.circular(14),
-            ),
+          const CarouselWidget(),
+          const ViewAllWidget(),
+          ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                height: 200,
+                width: 160,
+                decoration: BoxDecoration(color: whiteColor, borderRadius: BorderRadius.circular(12)),
+              )
+            ],
           )
+        ],
+      ),
+    );
+  }
+}
+
+class ViewAllWidget extends StatelessWidget {
+  const ViewAllWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 11),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Herbs Seasonings',
+            style: gFontsOleo(),
+          ),
+          Text(
+            'view all',
+            style: gFontsOleo(cl: greyColor),
+          ),
         ],
       ),
     );
