4dde6e4eebfdb00a0502445171e0a8d2ae6b03a4
MushthakVP <mail.musthak@gmai.com>
2022-08-11 18:43:50 +0530

    product container editing

--- lib/home_screen/view/home_screen.dart
+++ lib/home_screen/view/home_screen.dart
@@ -12,8 +12,7 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: scafoldColor,
       drawer: const Drawer(),
       appBar: AppBar(
-        backgroundColor: blackColor
-        ,
+        backgroundColor: blackColor,
         title: const Text('Home'),
         actions: const [
           CircleAvatar(
@@ -43,7 +42,7 @@ class HomeScreen extends StatelessWidget {
             children: [
               Container(
                 margin: const EdgeInsets.all(10),
-                height: 200,
+                height: 220,
                 width: 160,
                 decoration: BoxDecoration(
                   color: whiteColor,
@@ -53,7 +52,7 @@ class HomeScreen extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Expanded(
-                      flex: 2,
+                      flex: 4,
                       child: Center(
                         child: Image.network(
                           'https://assets.stickpng.com/images/58bf1e2ae443f41d77c734ab.png',
@@ -62,12 +61,14 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                     Expanded(
+                      flex: 3,
                       child: Padding(
                         padding: const EdgeInsets.symmetric(
                           horizontal: 8,
                           vertical: 8,
                         ),
                         child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
                               'Fresh Basil',
@@ -75,7 +76,35 @@ class HomeScreen extends StatelessWidget {
                                 fw: FontWeight.bold,
                               ),
                             ),
-                            const Text('Fresh Basil'),
+                            const Text('50\$/50 Gram'),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    height: 29,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      border: Border.all(color: greyColor),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Expanded(
+                                  child: Container(
+                                    height: 29,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      border: Border.all(color: greyColor),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ],
                         ),
                       ),
