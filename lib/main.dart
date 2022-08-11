bd8a7139a01079487d99fa2b7a8d707dd15a2ac4
MushthakVP <mail.musthak@gmai.com>
2022-08-11 15:05:00 +0530

    Home Screen Starting

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/home_screen/view/home_screen.dart';
 import 'package:food_delivery/routes/routes.dart';
-import 'package:food_delivery/splash_screen/view/splash_screen.dart';
+import 'package:food_delivery/sign_screen/viewmodel/auth_pov.dart';
 import 'package:food_delivery/splash_screen/viewmodel/splash_pov.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,15 +21,16 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<SplashPov>(create: (context) => SplashPov()),
+        ChangeNotifierProvider<AuthPov>(create: (context) => AuthPov(FirebaseAuth.instance)),
       ],
       child: MaterialApp(
         navigatorKey: Routes.navigatorKey,
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.teal,
+          primarySwatch: Colors.yellow,
         ),
-        home: const SplashScreen(),
+        home: const HomeScreen(),
       ),
     );
   }
