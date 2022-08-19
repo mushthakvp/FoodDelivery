6c016adb93baaffdfdf37d119df3e0f80b450b92
MushthakVP <mail.musthak@gmai.com>
2022-08-19 14:45:08 +0530

    search screen starting

--- lib/provider/providers.dart
+++ lib/provider/providers.dart
@@ -6,7 +6,6 @@ import '../favourite_screen/viewmodel/favourite_pov.dart';
 import '../home_screen/viewmodel/carousel_image_pov.dart';
 import '../home_screen/viewmodel/home_pov.dart';
 import '../product_overview_screen/viewmodel/addon_provider.dart';
-import '../search/viewmodel/search_pov.dart';
 import '../sign_screen/viewmodel/auth_pov.dart';
 import '../splash_screen/viewmodel/splash_pov.dart';
 
@@ -17,7 +16,6 @@ class Providers {
         ChangeNotifierProvider<SplashPov>(create: (context) => SplashPov()),
         ChangeNotifierProvider<AuthPov>(create: (context) => AuthPov(FirebaseAuth.instance)),
         ChangeNotifierProvider<HomePov>(create: (context) => HomePov()),
-        ChangeNotifierProvider<SearchScreenPov>(create: (context) => SearchScreenPov()),
         ChangeNotifierProvider<HomeCarouselPov>(create: (context) => HomeCarouselPov()),
         ChangeNotifierProvider<AddOnProductPov>(create: (context) => AddOnProductPov()),
         ChangeNotifierProvider<FavouritePov>(create: (context) => FavouritePov()),
---
+++ lib/search_screen/view/search_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/core/color/colors.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: cardColor,
+      ),
+    );
+  }
+}
