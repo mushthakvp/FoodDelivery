985731c5801bed31c51e7480de80250f6100df8e
MushthakVP <mail.musthak@gmai.com>
2022-08-20 10:13:45 +0530

    search widget created

--- lib/view_all_screen/view/viewall_screen.dart
+++ lib/view_all_screen/view/viewall_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../core/color/colors.dart';
 import '../viewmodel/view_provider.dart';
+import 'widget/custom_serch_widget.dart';
 
 class ViewAllScreen extends StatelessWidget {
   const ViewAllScreen({Key? key, required String collection}) : super(key: key);
@@ -10,6 +11,7 @@ class ViewAllScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: scafoldColor,
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(60),
           child: Consumer<ViewAllPov>(builder: (context, value, _) {
@@ -28,47 +30,6 @@ class ViewAllScreen extends StatelessWidget {
                   )
                 : const CustomSearchWidget();
           }),
-        ),
-      ),
-    );
-  }
-}
-
-class CustomSearchWidget extends StatelessWidget {
-  const CustomSearchWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: cardColor,
-      title: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        padding: const EdgeInsets.only(left: 10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: greyColor.withOpacity(.3),
-        ),
-        width: double.infinity,
-        child: const TextField(
-          style: TextStyle(
-            height: 1.3,
-            color: Colors.white,
-            letterSpacing: .8,
-          ),
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            suffixIcon: Icon(
-              Icons.close,
-              color: Colors.white,
-            ),
-            hintText: 'Search',
-            hintStyle: TextStyle(
-              color: greyColor,
-              fontSize: 16,
-            ),
-          ),
         ),
       ),
     );
