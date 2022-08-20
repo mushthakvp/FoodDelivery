e7ccec7fe3119655d8cb16c7af446d8b7ea84474
MushthakVP <mail.musthak@gmai.com>
2022-08-20 12:15:32 +0530

    search list added

--- lib/view_all_screen/view/viewall_screen.dart
+++ lib/view_all_screen/view/viewall_screen.dart
@@ -1,11 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/core/styles/fonts.dart';
 import 'package:provider/provider.dart';
 import '../../core/color/colors.dart';
+import '../model/view_all_model.dart';
 import '../viewmodel/view_provider.dart';
+import '../viewmodel/viewmodel_provider.dart';
+import 'widget/allitems_card.dart';
 import 'widget/custom_serch_widget.dart';
 
 class ViewAllScreen extends StatelessWidget {
-  const ViewAllScreen({Key? key, required String collection}) : super(key: key);
+  final String collection;
+  const ViewAllScreen({Key? key, required this.collection}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,22 +20,49 @@ class ViewAllScreen extends StatelessWidget {
         backgroundColor: scafoldColor,
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(60),
-          child: Consumer<ViewAllPov>(builder: (context, value, _) {
-            return value.searchValue
-                ? AppBar(
-                    backgroundColor: cardColor,
-                    actions: [
-                      IconButton(
-                        splashRadius: 26,
-                        onPressed: () {
-                          value.searchValueChange(false);
-                        },
-                        icon: const Icon(Icons.search),
+          child: Consumer<ViewAllPov>(
+            builder: (context, value, _) {
+              return value.searchValue
+                  ? AppBar(
+                      title: Text(
+                        'View All',
+                        style: gFontsOleo(cl: whiteColor, sz: 22),
                       ),
-                    ],
-                  )
-                : const CustomSearchWidget();
-          }),
+                      backgroundColor: cardColor,
+                      actions: [
+                        IconButton(
+                          splashRadius: 26,
+                          onPressed: () {
+                            value.searchValueChange(false);
+                          },
+                          icon: const Icon(Icons.search),
+                        ),
+                      ],
+                    )
+                  : const CustomSearchWidget();
+            },
+          ),
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection(collection).snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            List<ViewAllProductModel> list = ViewAllModelPov.convertToList(streamSnapshot);
+            return GridView.count(
+              physics: const BouncingScrollPhysics(),
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              crossAxisSpacing: 3,
+              childAspectRatio: 1 / 1.58,
+              children: List.generate(
+                list.length,
+                (index) {
+                  // final id = streamSnapshot.data!.docs[index];
+                  final data = list[index];
+                  return ViewAllItemsCard(data: data);
+                },
+              ),
+            );
+          },
         ),
       ),
     );
